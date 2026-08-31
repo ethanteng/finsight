2e4b0bedc6a657a3709c12ffb6b25fb07674527f
Ethan Teng <ethanteng@gmail.com>
2026-08-31 11:18:47 -0700

    Load responsive marketing hardening after base styles

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import '../components/marketing/marketing.css'
+import '../components/marketing/marketing-responsive.css'
 import StructuredData from '../components/StructuredData'
 import type { Metadata } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -73,7 +74,7 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              new Date().getTime(),event:'gtm.js');var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PL362L36');
